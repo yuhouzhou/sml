060660062eb70a91789e2f4a75ea0fbafccf67e9
yuhouzhou <yu.zhou@jacobs-university.de>
2019-09-17 17:16:38 +0200

    add p1 temp

---
+++ HW02_2019_09_16/p1.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def bisection(n, c, p):
+    pass
+
+def newton(n, c, p):
+    pass
+
+def secant(n, c, p):
+    pass
+
+def brent(n, c, p):
+    pass
+
+if __name__ == '__main__':
+    pass
