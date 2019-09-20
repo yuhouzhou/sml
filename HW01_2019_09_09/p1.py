491c768eb05f9a699158ea5ee7237ddc288c2367
yuhouzhou <zyh.germany@gmail.com>
2019-09-20 23:58:01 +0200

    my_bisect finish

--- HW01_2019_09_09/p1.py
+++ HW01_2019_09_09/p1.py
@@ -1,48 +1,54 @@
 import numpy as np
 import timeit
 
+
 # explicit loop
 def w1(c_lst, x):
     pv = 0
     for i, c in enumerate(c_lst):
-        pv += c * x**(i+1)
+        pv += c * x ** (i + 1)
     return pv
+
 
 # Horner's scheme
 def w2(c_lst, x):
     c = c_lst[0]
     if len(c_lst) > 1:
         c_lst = c_lst[1:]
-        pv = (c + w2(c_lst, x))*x
+        pv = (c + w2(c_lst, x)) * x
     else:
-        return c*x
+        return c * x
     return pv
+
 
 def w5(c_lst, x):
     pv = 0
-    for i in np.arange(len(c_lst)-1, -1, -1):
-        pv = c_lst[i]+(x*pv)
+    for i in np.arange(len(c_lst) - 1, -1, -1):
+        pv = c_lst[i] + (x * pv)
     pv *= x
     return pv
+
 
 # polyval
 def w3(c_lst, x):
     c_lst = np.append(c_lst[::-1], 0)
     return np.polyval(c_lst, x)
 
+
 # dot product
 def w4(c_lst, x):
     i = np.arange(1, len(c_lst) + 1)
     return np.dot(c_lst, x ** i)
+
 
 if __name__ == "__main__":
     c_lst = 100 * np.arange(1000, 1800)
     r = 0.03
     x = 1 / (1 + r)
 
-    w_dic = {'explicit loop':w1, "Horner's scheme (Recursive)":w2,
+    w_dic = {'explicit loop': w1, "Horner's scheme (Recursive)": w2,
              "Horner's scheme (Iterative)": w5,
-             "polyval":w3, "dot product":w4}
+             "polyval": w3, "dot product": w4}
 
     for k, v in w_dic.items():
         t = timeit.Timer('{}(c_lst, x)'.format(v.__name__), 'from __main__ import {},c_lst,x'.format(v.__name__))
--- HW02_2019_09_16/p1.py
+++ HW02_2019_09_16/p1.py
@@ -1,16 +1,59 @@
 import numpy as np
+from scipy.optimize import bisect, newton, brent
+from sys import exit
 
-def bisection(n, c, p):
+
+def my_bisect(f, a=0, b=1, tol=1e-12, maxiter=100):
+    """
+
+    :param f: python function return a number
+    :param a: one side of the start interval
+    :param b: the other side of the start interval
+    :param tol: tolerance about the precision
+    :param maxiter: maximum iteration
+    :return: the root
+    """
+    # assume a < b
+    if a > b:
+        tmp = a
+        a = b
+        b = tmp
+
+    for i in range(1, maxiter + 1):
+        # mid point of (a, b)
+        x = (a + b) / 2
+        f_x = f(x)
+        if f_x == 0 or (b - a) / 2 < tol:
+            return x
+        if f(a) * f_x > 0:
+            a = x
+        else:
+            b = x
+        if i == maxiter:
+            print("Maximum iteration reached!")
+            exit(1)
+
+
+def my_newton(f, x0=0.5, tol=1.48e-08, maxiter=50):
     pass
 
-def newton(n, c, p):
+
+def secant(poly):
     pass
 
-def secant(n, c, p):
+
+def my_brent(poly):
     pass
 
-def brent(n, c, p):
-    pass
 
 if __name__ == '__main__':
-    pass
+    def f(x):
+        n = 300
+        c, p = 120 * np.arange(10, n + 10), 15000
+        coe = c[::-1]
+        index = np.arange(1, len(c) + 1)[::-1]
+        f_x = np.dot(coe, x ** index) - p
+        return f_x
+
+
+    print(my_bisect(f), bisect(f, 0, 1))
