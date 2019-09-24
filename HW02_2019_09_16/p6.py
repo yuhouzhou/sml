7369c432e2933c4fffe9d53a25985ffd5b8423cc
yuhouzhou <yu.zhou@jacobs-university.de>
2019-09-24 17:49:46 +0200

    add some steps

---
+++ HW02_2019_09_16/p6.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from p2 import pv
+
+if __name__ == '__main__':
+    r = 0.08
+    c = 0.08
+    n = 15
+    m = 2
+    f = 1
+
+    period = np.arange(1, m * n + 1)
+
+    r_lst_lst = {'r: 6% - 8%': np.linspace(0.06, r, n * m),
+                 'r: 8% - 8%' : np.linspace(r, r, n * m),
+                 'r: 8% - 10%' : np.linspace(r, 0.1, n * m)}
+
+    for i, (key ,r_lst) in enumerate(r_lst_lst.items()):
+        forward_v_lst = [price * (1 + r / m) ** (i + 1) for price in [pv(irr=r, c=c, m=m, n=n, f=f) for r in r_lst]]
+        plt.plot(np.multiply(period, 1/2), forward_v_lst, label='${}$'.format(key.replace('%', '\%')))
+        plt.xlabel("Time to Maturity (Year)")
+        plt.ylabel("Forward Value")
+        plt.title("Forward Value vs. Time to Maturity")
+        plt.legend()
+
+    plt.tight_layout()
+    plt.savefig('p6.pdf')
+    plt.show()
---
+++ HW02_2019_09_16/p7.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import brent
+
+if __name__ == '__main__':
+    n = 30
+    c = 0.1
+    m = 1
+    f = 1
+    horizon = 10
+
+    fv
