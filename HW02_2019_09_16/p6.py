040dea2b4b77bb4207f6e5907db6aa60d0d5af9d
yuhouzhou <yu.zhou@jacobs-university.de>
2019-09-27 16:15:47 +0200

    fix sth

--- HW02_2019_09_16/p6.py
+++ HW02_2019_09_16/p6.py
@@ -3,7 +3,18 @@ import matplotlib.pyplot as plt
 from p2 import pv
 
 def forward_v(r, c, m, n, f, i):
-    return pv(irr=r, c=c, m=m, n=n, f=f) * (1 + r / m) ** (i + 1)
+    """
+
+    :param r: yield to maturity
+    :param c: annual coupon rate
+    :param m: number of payments per year
+    :param n: time to maturity
+    :param d: discount rate
+    :param f: par value
+    :param i: period
+    :return: forward value
+    """
+    return pv(irr=r, c=c, m=m, n=n, f=f) * (1 + r / m) ** (i)
 
 if __name__ == '__main__':
     r = 0.08
@@ -17,18 +28,18 @@ if __name__ == '__main__':
     # r_lst_dict = {'r: 6% - 8%': np.linspace(r, 0.06,  n * m),
     #              'r: 8% - 8%' : np.linspace(r, r, n * m),
     #              'r: 8% - 10%' : np.linspace(r, 0.1, n * m)}
-
-    # for i, (key ,r_lst) in enumerate(r_lst_dict.items()):
-    #     forward_v_lst = [price * (1 + r / m) ** (i + 1) for price in [pv(irr=r, c=c, m=m, n=n, f=f) for r in r_lst]]
-    #     plt.plot(np.multiply(period, 1/2), forward_v_lst, label='${}$'.format(key.replace('%', '\%')))
+    #
+    # for key ,r_lst in r_lst_dict.items():
+    #     forward_v_lst = [pv(irr=r, c=c, m=m, n=n, f=f) * (1 + r / m) ** (i + 1)  for i, r in enumerate(r_lst)]
+    #     plt.plot(np.multiply(periods, 1/2), forward_v_lst, label='${}$'.format(key.replace('%', '\%')))
 
     for r in [0.06, 0.08, 0.1]:
         forward_v_lst = [forward_v(r, c, m, n, f, i) for i in periods]
         plt.plot(np.multiply(periods, 1 / 2), forward_v_lst, label='$r = {}$'.format(r))
 
-    plt.xlabel("Time to Maturity (Years)")
+    plt.xlabel("Years to Maturity")
     plt.ylabel("Forward Value")
-    plt.title("Forward Value vs. Time to Maturity")
+    plt.title("Forward Value vs. Years to Maturity")
     plt.legend()
     plt.tight_layout()
     plt.savefig('p6.pdf')
